af61c15ff515388bda26922b46997b7a7ae53f11
Nagendra1432 <nagendramejari@gmail.com>
2025-01-20 11:58:14 +0530

    message

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -86,7 +86,7 @@ const navigate = useNavigate()
                 value={formiksignIN.values.email}
                 type="text"
                 name="email"
-                placeholder="user@gmail.com"
+                placeholder="john@gmail.com"
                 className="px-3 py-2 mt-4 border-b-[1px] border-black w-full sm:w-2/3 focus:outline-none"
               />
               {formiksignIN.errors.email && formiksignIN.touched.email ? (
